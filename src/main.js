0e3eb35ba25a1190b0894d75578890048ce1b2ba
sgtflame <linterra@gmail.com>
2016-01-23 16:45:20 -0500

    Started adding zscript
    ZScript is based on Lisp/Clojure, and the implementation is derived from
    the Mal implementation

--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
 #! /usr/bin/env node
-var times = 0;
-function main() {
 
-  console.log('Generating...');
-  var GeneratorManager = require('./generator.js');
+console.log('Generating...');
+var GeneratorManager = require('./generator.js');
 
-  var meta = require('./meta/main.json');
+var meta = require('./meta/main.json');
 
-  var generator = GeneratorManager.getService('nodejs');
-
-  generator.generate();
-}
-
-if(times == 0) {
-  times = times + 1;
-  main();
-}
+//var generator = GeneratorManager.getService('nodejs');
+//generator.generate();
